b24e922e69e0022645d422db6ae3414da72989b9
Ivan <ivanenoriega@gmail.com>
2020-05-30 16:54:02 -0300

    Create email handler added

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
+	"regexp"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/heroku/x/hmetrics/onload"
 	"github.com/ivanenoriega/el-dorado-api/db"
+	m "github.com/ivanenoriega/el-dorado-api/mapper/email"
+	mo "github.com/ivanenoriega/el-dorado-api/model/email"
 )
 
 func main() {
@@ -19,23 +22,37 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": db.Hello(),
+			"message": "Pong!",
 		})
 	})
 
 	router.POST("/email", func(c *gin.Context) {
+		var data mo.ReqEmail
+		con := db.GetConn()
+		if err := c.Bind(&data); err != nil {
+			c.JSON(400, gin.H{"error": "invalid email"})
+			return
+		}
+		if data.Email == "" {
+			c.JSON(400, gin.H{"error": "empty email"})
+			return
+		}
+		re := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+		if re.MatchString(data.Email) == false {
+			c.JSON(400, gin.H{"error": "malformed email"})
+			return
+		}
+		err := m.Insert(data.Email, con)
+		if err != nil {
+			panic(err.Error())
+		}
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "Success",
 		})
-	})
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		defer con.Close()
 	})
 
 	router.Run(":" + port)
