27c717568a6890baf72a2c54e9754ca2bb7e7f4c
Ivan <ivanenoriega@gmail.com>
2020-05-30 10:03:27 -0300

    ping and email endpoints added

--- main.go
+++ main.go
@@ -21,6 +21,12 @@ func main() {
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
