2a204b290e3772dd2f17625ebba2afb4c2d23b71
Ivan <ivanenoriega@gmail.com>
2020-05-30 23:47:02 -0300

    create contact table query added

--- db/db.sql
+++ db/db.sql
@@ -1,8 +1,18 @@
-CREATE SCHEMA `el-dorado`
-USE `el-dorado`
+DROP SCHEMA `el-dorado`;
+CREATE SCHEMA `el-dorado`;
+USE `el-dorado`;
 
-CREATE TABLE `el-dorado`.`email` (
+CREATE TABLE email (
   `id` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`, `email`));
+
+CREATE TABLE contact (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `phone` VARCHAR(45) NOT NULL,
+  `subject` VARCHAR(45) NOT NULL,
+  `message` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`));
 
